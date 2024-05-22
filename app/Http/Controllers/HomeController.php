dcd1f960c334b6bdce6dfa8cb729f1e5889ba397
Jimmy <toluadejimi@gmail.com>
2024-05-22 09:47:21 +0100

    pushhhhhh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1167,15 +1167,16 @@ class HomeController extends Controller
     public function diasy_webhook(request $request)
     {
 
-//        $activationId = $request->orderid;
-//        $messageId = $request->messageId;
-//        $service = $request->service;
-//        $text = $request->text;
-//        $code = $request->sms;
-//        $country = $request->country;
-//        $receivedAt = $request->receivedAt;
-//
-//        $orders = Verification::where('order_id', $activationId)->update(['sms' => $code]);
+
+        $activationId = $request->activationId;
+        $messageId = $request->messageId;
+        $service = $request->service;
+        $text = $request->text;
+        $code = $request->sms;
+        $country = $request->country;
+        $receivedAt = $request->receivedAt;
+
+        $orders = Verification::where('order_id', $activationId)->update(['sms' => $code, 'status'=>2]);
 
         $message = json_encode($request->all());
         send_notification($message);
