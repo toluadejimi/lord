db2fc1218c73736e550caf20074c655f346dff62
Jimmy <toluadejimi@gmail.com>
2024-09-11 09:28:17 +0100

    pushhhhhh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -550,7 +550,7 @@ class HomeController extends Controller
 
 
             $user = Auth::id() ?? null;
-            return redirect('home');
+            return redirect('cworld');
         }
 
         return back()->with('error', "Email or Password Incorrect");
