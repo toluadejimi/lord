96c254a865d31cadca56bb84b71654b892282376
Jimmy <toluadejimi@gmail.com>
2024-10-15 09:13:13 +0100

    pushhhhhh

---
+++ app/Http/Middleware/CheckSession.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckSession
+{
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            if ($user->session_id !== session()->getId()) {
+                // If session IDs do not match, log out the user
+                Auth::logout();
+                return redirect('/login')->withErrors('You have been logged out due to another login.');
+            }
+        }
+
+        return $next($request);
+    }
+}
