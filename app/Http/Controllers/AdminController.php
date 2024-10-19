478f48e9d23357659257c3f57ca9007c5a5db087
Jimmy <toluadejimi@gmail.com>
2024-10-19 05:28:59 +0100

    pushhhhhh

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -253,14 +253,15 @@ class AdminController extends Controller
         }
 
 
-        $user = User::where('id', $request->id)->first();
-        $transaction= Transaction::latest()->where('user_id', $request->id)->paginate();
-        $verification = verification::latest()->where('user_id', $request->id)->paginate();
+        $data['user'] = User::where('id', $request->id)->first();
+        $data['transaction']= Transaction::latest()->where('user_id', $request->id)->paginate(50);
+        $data['verification'] = verification::latest()->where('user_id', $request->id)->paginate(50);
 
+        $data['total_funded'] = Transaction::where('user_id', $request->id)->where('status', 2)->sum('amount');
+        $data['total_bought'] = verification::where('user_id', $request->id)->where('status', 2)->sum('cost');
+        $data['total_balance'] = $data['total_funded'] -  $data['total_bought'];
 
-        return view('view-user', compact('user', 'transaction','verification' ));
-
-
+        return view('view-user',$data);
     }
 
 
