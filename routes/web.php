b6363f3705ff96575e04673dfca94e4b48d5575f
Jimmy <toluadejimi@gmail.com>
2024-10-19 05:43:56 +0100

    pushhhhhh

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::get('login',  [HomeController::class,'login_index'])->name('login');
 Route::post('register_now',  [HomeController::class,'register']);
 Route::get('register',  [HomeController::class,'register_index']);
 Route::post('user-ban',  [HomeController::class,'user_ban']);
-Route::any('ban-users',  [HomeController::class,'ban_users']);
+Route::any('ban-user',  [HomeController::class,'ban_users']);
 Route::any('unban-users',  [HomeController::class,'unban_users']);
 
 
